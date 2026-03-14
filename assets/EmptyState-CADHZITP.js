96b00eb7a70c85d645212c018d3b6cc1b6e2ab20
RuizDiazAlexander <af.alexander.rd@gmail.com>
2026-03-14 02:25:59 -0300

    Updates

---
+++ assets/EmptyState-CADHZITP.js
@@ -0,0 +1,6 @@
+import{c as s,u as c,j as e}from"./index-2M2sEO2K.js";/**
+ * @license lucide-react v0.359.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=s("SearchX",[["path",{d:"m13.5 8.5-5 5",key:"1cs55j"}],["path",{d:"m8.5 8.5 5 5",key:"a8mexj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]),n=()=>{const{t}=c();return e.jsxs("div",{className:"col-span-12 flex flex-col items-center justify-center rounded-lg border border-[#1E1E2E] bg-[#12122A] p-12 text-center",children:[e.jsx(a,{size:48,className:"mb-4 text-[#94A3B8]"}),e.jsx("h3",{className:"mb-2 text-xl font-bold text-[#F1F5F9]",children:t("empty.no_anime")}),e.jsx("p",{className:"text-[#94A3B8]",children:t("empty.suggestion")})]})};export{n as E};
